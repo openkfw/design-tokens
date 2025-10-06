43db2373fc41e9f6fe5068cdbc22603408e98727
artursopelnik <artur.sopelnik93@gmail.com>
2025-10-06 19:13:40 +0200

    fix: Fix demo urls

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite"
 import postcssCustomMedia from "postcss-custom-media"
 
 export default defineConfig({
+  base: "https://openkfw.github.io/design-tokens/demo/dist/",
   css: {
     postcss: {
       plugins: [postcssCustomMedia()]
